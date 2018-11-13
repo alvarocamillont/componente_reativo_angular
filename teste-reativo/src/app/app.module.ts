252b3e040575d2069f89c8a951d8ed0e1324715d
Alvaro Camillo Neto <alvaro.camilloneto@gmail.com>
2018-11-13 10:21:13 -0200

    Criação do componente

--- teste-reativo/src/app/app.module.ts
+++ teste-reativo/src/app/app.module.ts
@@ -2,15 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RatingInputComponent } from './rating-input/rating-input.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, RatingInputComponent],
+  imports: [BrowserModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ teste-reativo/src/app/rating-input/rating-input.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rating-input',
+  templateUrl: './rating-input.component.html',
+  styleUrls: ['./rating-input.component.css']
+})
+export class RatingInputComponent {
+
+  stars: boolean[] = Array(5).fill(false);
+
+  get value(): number {
+    return this.stars.reduce((total, starred) => {
+      return total + (starred ? 1 : 0);
+    }, 0);
+  }
+
+  rate(rating: number) {
+    this.stars = this.stars.map((_, i) => rating > i);
+  }
+
+}
