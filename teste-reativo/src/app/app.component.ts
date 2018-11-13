27ffbe446748057a969bf1cf021fa7f557afb96b
Alvaro Camillo Neto <alvaro.camilloneto@gmail.com>
2018-11-13 11:18:35 -0200

    Correção para transformar o componente em um componente reativo

--- teste-reativo/src/app/app.component.ts
+++ teste-reativo/src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  formulario: FormGroup;
   title = 'teste-reativo';
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit() {
+    this.formulario = this.formBuilder.group({
+      filme: [null],
+      nota: [null]
+    });
+  }
+
+  onSubmit() {
+    const valueSubmit = Object.assign({}, this.formulario.value);
+
+    console.log(valueSubmit);
+
+    if (this.formulario.valid) {
+      alert('Está certo');
+    } else {
+      alert('Está errado');
+    }
+  }
 }
